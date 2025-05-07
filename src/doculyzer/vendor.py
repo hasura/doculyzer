2184eefdc783aca8dac957219a295db00f56eb38
kenstott <kenneth.stott@hasura.io>
2025-05-07 22:21:41 +0000

    initial commit

--- src/doculyzer/vendor.py
+++ src/doculyzer/vendor.py
@@ -10,20 +10,7 @@ def get_vendor_path():
     """
     # Determine the current module directory
     package_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # Debug mode: Check if two levels up contains `vendor/libmagic`
-    debug_vendor_path = os.path.abspath(os.path.join(package_dir, '..', '..', 'vendor', 'libmagic'))
-
-    # Installed mode: Check if `vendor/libmagic` exists in the current directory
-    installed_vendor_path = os.path.join(package_dir, 'vendor', 'libmagic')
-
-    # Prioritize debug path if it exists
-    if os.path.exists(debug_vendor_path):
-        return debug_vendor_path
-
-    # Fallback to the installed path
-    if os.path.exists(installed_vendor_path):
-        return installed_vendor_path
-
-    # If neither path exists, raise an error
-    raise FileNotFoundError("The 'vendor/libmagic' directory could not be located.")
+    vendor_path = os.path.join(package_dir, 'vendor', 'libmagic')
+    if os.path.exists(vendor_path):
+        return vendor_path
+    raise FileNotFoundError(f"The 'vendor/libmagic' directory could not be located.: {vendor_path}")
